f0722e10d4a9a65cb65a621b4a716e37e8f1c0f6
engrosamaali91 <engr.osamaali91@gmail.com>
2025-08-14 16:35:57 +0200

    localize python file

--- src/nav2_test.py
+++ src/nav2_test.py
@@ -1,37 +1,40 @@
-#!/usr/bin/env python3
-import rclpy
-import tf_transformations
+    #!/usr/bin/env python3
 
-from nav2_simple_commander.robot_navigator import BasicNavigator
-from geometry_msgs.msg import PoseStamped
+    import rclpy
+    import tf_transformations
 
-
-def main():
-    # initialize rclpy
-    rclpy.init()
-    nav = BasicNavigator()
-
-    # set initial pose
-    q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, 0.0)
-    initial_pose = PoseStamped()
-    initial_pose.header.frame_id = 'map'
-    initial_pose.header.stamp = nav.get_clock().now().to_msg()
-    initial_pose.pose.position.x = 0.0
-    initial_pose.pose.position.y = 0.0
-    initial_pose.pose.position.z = 0.0
-    initial_pose.pose.orientation.x = q_x
-    initial_pose.pose.orientation.y = q_y
-    initial_pose.pose.orientation.z = q_z
-    initial_pose.pose.orientation.w = q_w
-    nav.setInitialPose(initial_pose)
+    from nav2_simple_commander.robot_navigator import BasicNavigator
+    from geometry_msgs.msg import PoseStamped
+    from scipy.spatial.transform import Rotation as R
 
 
-    #-- wait for nav2 
-    nav.waitUntilNav2Active()
+    def main():
+        # initialize rclpy
+        rclpy.init()
+        nav = BasicNavigator()
 
-    rclpy.shutdown()
-    
+        # set initial pose
+        q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, 0.0)
+        initial_pose = PoseStamped()
+        initial_pose.header.frame_id = 'map'
+        initial_pose.header.stamp = nav.get_clock().now().to_msg()
+        initial_pose.pose.position.x = 0.0
+        initial_pose.pose.position.y = 0.0
+        initial_pose.pose.position.z = 0.0
+        initial_pose.pose.orientation.x = q_x
+        initial_pose.pose.orientation.y = q_y
+        initial_pose.pose.orientation.z = q_z
+        initial_pose.pose.orientation.w = q_w
+
+        nav.setInitialPose(initial_pose)
 
 
-if __name__ == '__main__':
-    main()
+        #-- wait for nav2 
+        nav.waitUntilNav2Active()
+
+        rclpy.shutdown()
+        
+
+
+    if __name__ == '__main__':
+        main()
